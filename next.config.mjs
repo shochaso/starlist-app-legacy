6a562a16a21f04ba9e2dd05762010b647dfc16ab
shochaso <shochaso@gmail.com>
2025-12-15 04:14:30 +0900

    Redirect root to /teaser

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@ const config = {
   typescript: {
     tsconfigPath: "./next-tsconfig.json",
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/teaser",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default config;
